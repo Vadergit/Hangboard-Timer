83a7a0f5583fe88b98aa7b496255f8e32ceeb0ac
Dario Neuenschwander <110724880+Vadergit@users.noreply.github.com>
2022-11-13 00:57:21 +0100

    Update Hangboard_Test_incl_Timer.ino

--- Hangboartimer/Hangboardtimer V2 T-display/Arduino/Hangboard_Test_incl_Timer.ino
+++ Hangboartimer/Hangboardtimer V2 T-display/Arduino/Hangboard_Test_incl_Timer.ino
@@ -53,7 +53,7 @@ unsigned long usertime;
 int counter =3;
 #define ROTARY_ENCODER_A_PIN 16
 #define ROTARY_ENCODER_B_PIN 3
-#define ROTARY_ENCODER_BUTTON_PIN 21
+#define ROTARY_ENCODER_BUTTON_PIN 1
 #define ROTARY_ENCODER_VCC_PIN -1 /* 27 put -1 of Rotary encoder Vcc is connected directly to 3,3V; else you can use declared output pin for powering rotary encoder */
 
 //depending on your encoder - try 1,2 or 4 to get expected behaviour
@@ -98,6 +98,21 @@ void rotary_loop()
 	if (rotaryEncoder.isEncoderButtonClicked())
 	{
 		rotary_onButtonClick();
+    tft.fillScreen(TFT_BLACK);
+  
+  tft.drawBitmap(TFTWITH/2 - 36/2, TFTHEIGHT/2 - 48, epd_bitmap_Palm, 36, 48, TFT_WHITE);
+  tft.drawBitmap(TFTWITH/2 - ((2 * 36) - 4)/2, TFTHEIGHT/2 - 48, epd_bitmap_Palml, 36, 48, TFT_WHITE);
+   delay(1000);
+  tft.drawBitmap(TFTWITH/2 - 36/2, TFTHEIGHT/2 - 48, epd_bitmap_Mittel, 36, 48, TFT_WHITE);
+  tft.drawBitmap(TFTWITH/2 - ((2 * 36) - 4)/2, TFTHEIGHT/2 - 48, epd_bitmap_Mittell, 36, 48, TFT_WHITE);
+  delay(1000);
+  tft.drawBitmap(TFTWITH/2 - 36/2, TFTHEIGHT/2 - 48, epd_bitmap_Mittel, 36, 48, TFT_BLACK);
+  tft.drawBitmap(TFTWITH/2 - ((2 * 36) - 4)/2, TFTHEIGHT/2 - 48, epd_bitmap_Mittell, 36, 48, TFT_BLACK);
+  delay(1000);
+  tft.drawBitmap(TFTWITH/2 - 36/2, TFTHEIGHT/2 - 48, epd_bitmap_Mittel, 36, 48, TFT_WHITE);
+  tft.drawBitmap(TFTWITH/2 - ((2 * 36) - 4)/2, TFTHEIGHT/2 - 48, epd_bitmap_Mittell, 36, 48, TFT_WHITE);
+  delay(1000);
+tft.fillScreen(TFT_BLACK);
 	}
 }
 void IRAM_ATTR readEncoderISR()
@@ -110,15 +125,16 @@ void setup(void) {
 
   pinMode(21, INPUT_PULLUP);
 
-  pinMode(3, INPUT);
-  pinMode(16, INPUT);
+ 
+ /*
+  
   pinMode(15, OUTPUT);  //battery enable
   digitalWrite(15, 1);  //battery enable
   ledcSetup(0, 10000, 8);
   ledcAttachPin(38, 0);
   ledcWrite(0, brightness);
 
-
+*/
 
 
 //we must initialize rotary encoder
