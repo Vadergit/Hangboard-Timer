71de934b810d5a1100bbbc6a24272e7b1c2ef350
Dario Neuenschwander <110724880+Vadergit@users.noreply.github.com>
2023-01-11 12:42:45 +0100

    Create hostname.local.ino

---
+++ Hangboartimer/Hangboardtimer V2 T-display/Arduino/hostname.local.ino
@@ -0,0 +1,10 @@
+MDNS.begin ( hostName );
+
+MDNS.addService("http", "tcp", 80);
+
+
+https://techtutorialsx.com/2020/04/17/esp32-mdns-address-resolution/
+
+https://github.com/espressif/arduino-esp32/blob/master/libraries/ESPmDNS/examples/mDNS_Web_Server/mDNS_Web_Server.ino
+
+
